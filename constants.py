c619a13a737411bb2d7cd1cb0e0660a5924d340d
Shayan Shabanzadeh <shayan.shabanzadeh@peykasa.ir>
2024-01-29 18:30:28 +0330

    refactor: add some constants and remove some useless parameters

--- constants.py
+++ constants.py
@@ -1,5 +1,16 @@
 # LCG parameters
+from ApplicationSubmissionQueue import ApplicationSubmissionQueue
+from ComplaintsQueue import ComplaintsQueue
+from CoreA import CoreA
+from CoreB import CoreB
+from CoreC import CoreC
+from DocumentVerificationQueue import DocumentVerificationQueue
+from ReviewRequestQueue import ReviewRequestQueue
+from WritingQueue import WritingQueue
+
 seed = 42
-a = 1664525
-c = 1013904223
-m = 2 ** 32
+# a = 1664525
+# c = 1013904223
+# m = 2 ** 32
+CORE_TYPES = [CoreA, CoreB, CoreC]
+QUEUE_TYPES = [WritingQueue, ComplaintsQueue, DocumentVerificationQueue, ApplicationSubmissionQueue, ReviewRequestQueue]
