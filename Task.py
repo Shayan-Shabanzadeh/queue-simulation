212c0cebd9ff69a47f02711dfc8aa7af1ca2fb67
Shayan Shabanzadeh <shayan.shabanzadeh@peykasa.ir>
2024-01-29 19:22:47 +0330

    refactor: change task to have more information

--- Task.py
+++ Task.py
@@ -6,3 +6,14 @@ class Task:
         self.interarrival_time = interarrival_time
         self.service_time = service_time
         self.id = uuid.uuid4()
+        self.start_execution_time = 0
+        self.end_execution_time = 0
+        self.time_in_the_queue = 0
+        self.arrival_time = 0
+
+    def __str__(self):
+        return f"Task ID: {self.id}, Interarrival Time: {self.interarrival_time}, " \
+               f"Service Time: {self.service_time}, Arrival Time: {self.arrival_time}, " \
+               f"Start Execution Time: {self.start_execution_time}, " \
+               f"End Execution Time: {self.end_execution_time}, " \
+               f"Time in the Queue: {self.time_in_the_queue}"
